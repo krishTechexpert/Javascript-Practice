7dd26930e50e8ebc04e7c6adc89ec0074a85c8cb
krishna kumar <68417802+krishTechexpert@users.noreply.github.com>
2023-08-08 17:48:24 +0530

    Update demo-2.js

--- Memoization/demo-2.js
+++ Memoization/demo-2.js
@@ -1,8 +1,5 @@
 // example for passing multiple arguments
-
-
 function calculation(num){
-
 let sum=0;
   for(let i=1;i<=num.length;i++){
     sum+=i;
@@ -10,13 +7,10 @@ let sum=0;
   return sum
 }
 
-
 const memoization =(fn) =>{
   let cache={}
-
   return function(...args){
     const num=JSON.stringify(args)
-
     if(cache[num]){
       console.log('take value from cache',num)
       return cache[num]
@@ -26,18 +20,11 @@ const memoization =(fn) =>{
       cache[num]=val;
       return val
     }
-
   }
-
 }
-
-
 const result=(memoization(calculation))
-
 console.log(result(1,2,3,4,5,6,7,8,9,10))
-
 console.log(result(1,2,3,4,5,6,7,8,9,10))
-
 // calculating...
 // 55
 // take value from cache [1,2,3,4,5,6,7,8,9,10]
